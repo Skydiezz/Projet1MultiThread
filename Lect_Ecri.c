7120a6640a3dbedb9a003acec3f5cb53ef1cd4f3
Gabriel Dhom Nascimento <dhomnascimen@didac09.info.ucl.ac.be>
2024-12-02 17:05:00 +0100

    1.3

--- Lect_Ecri.c
+++ Lect_Ecri.c
@@ -1,139 +1,121 @@
-#include <pthread.h>
-#include <semaphore.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <pthread.h>
+#include <semaphore.h>
 
-#define NB_LECTURES 2540
-#define NB_ECRITURES 640
 
-int readers = 0;                 
-int waiting_writers = 0;         
-pthread_mutex_t mutex;            
-sem_t room_empty;                 
-sem_t turnstile;                 
+int cmp_red = 0;       
+int cmp_leci = 0;      
+sem_t mutex_cmp_lec;   
+sem_t mutex_cmp_ecr;  
+sem_t critique;           
+sem_t queue;              
 
-void* lecteur(void* arg) {
-    int id = *(int*)arg;
-    free(arg);
 
-    for (int i = 0; i < NB_LECTURES; i++) {
-        sem_wait(&turnstile);           
-        sem_post(&turnstile);
+void rienfaire() {
+    for (int i = 0; i < 10000; i++);
+}
 
-        pthread_mutex_lock(&mutex);
-        readers++;
-        if (readers == 1) {            
-            sem_wait(&room_empty);
+void *lec(void *arg) {
+    int id = *(int *)arg;
+
+    for (int i = 0; i < 2540; i++) {
+        sem_wait(&queue);          
+        sem_wait(&mutex_cmp_lec);
+        if (cmp_red == 0) {
+            sem_wait(&critique);   
         }
-        pthread_mutex_unlock(&mutex);
+        cmp_red++;
+        sem_post(&mutex_cmp_lec);
+        sem_post(&queue);         
 
         
-        printf("Lecteur %d lit. (%d/2540)\n", id, i + 1);
-        for (int j = 0; j < 10000; j++); 
+        printf("Lecteur %d lit les données.\n", id);
+        rienfaire();
 
-        pthread_mutex_lock(&mutex);
-        readers--;
-        if (readers == 0) {            
-            sem_post(&room_empty);
+        sem_wait(&mutex_cmp_lec);
+        cmp_red--;
+        if (cmp_red == 0) {
+            sem_post(&critique);
         }
-        pthread_mutex_unlock(&mutex);
+        sem_post(&mutex_cmp_lec);
     }
-
     return NULL;
 }
 
-void* ecrivain(void* arg) {
-    int id = *(int*)arg;
-    free(arg);
 
-    for (int i = 0; i < NB_ECRITURES; i++) {
-        pthread_mutex_lock(&mutex);
-        waiting_writers++;             
-        pthread_mutex_unlock(&mutex);
+void *writer(void *arg) {
+    int id = *(int *)arg;
 
-        sem_wait(&turnstile);          
-        sem_wait(&room_empty);         
-
-        
-        printf("Écrivain %d écrit. (%d/640)\n", id, i + 1);
-        for (int j = 0; j < 10000; j++); 
-
-        sem_post(&room_empty);         
-
-        pthread_mutex_lock(&mutex);
-        waiting_writers--;             
-        if (waiting_writers == 0) {    
-            sem_post(&turnstile);
+    for (int i = 0; i < 640; i++) {
+        sem_wait(&mutex_cmp_ecr);
+        cmp_leci++;
+        if (cmp_leci == 1) {
+            sem_wait(&queue);     
         }
-        pthread_mutex_unlock(&mutex);
-    }
+        sem_post(&mutex_cmp_ecr);
 
+        sem_wait(&critique);       
+        
+        printf("Écrivain %d écrit des données.\n", id);
+        rienfaire();
+        sem_post(&critique);
+
+        sem_wait(&mutex_cmp_ecr);
+        cmp_leci--;
+        if (cmp_leci == 0) {
+            sem_post(&queue);      
+        }
+        sem_post(&mutex_cmp_ecr);
+    }
     return NULL;
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
     if (argc != 3) {
-        fprintf(stderr, "Usage: %s <nombre_lecteurs> <nombre_ecrivains>\n", argv[0]);
+        fprintf(stderr, "Usage: %s <nombre_lecteurs> <nombre_lecivains>\n", argv[0]);
         return EXIT_FAILURE;
     }
 
-    int nb_lecteurs = atoi(argv[1]);
-    int nb_ecrivains = atoi(argv[2]);
+    int nb_lec = atoi(argv[1]);
+    int nb_ecr = atoi(argv[2]);
 
-    if (nb_lecteurs <= 0 || nb_ecrivains <= 0) {
-        fprintf(stderr, "Le nombre de lecteurs et d'écrivains doit être > 0.\n");
-        return EXIT_FAILURE;
+    pthread_t lecs[nb_lec];
+    pthread_t ecr[nb_ecr];
+    int ids_lec[nb_lec];
+    int ids_ecr[nb_ecr];
+
+    sem_init(&mutex_cmp_lec, 0, 1);
+    sem_init(&mutex_cmp_ecr, 0, 1);
+    sem_init(&critique, 0, 1);
+    sem_init(&queue, 0, 1);
+
+    for (int i = 0; i < nb_lec; i++) {
+        ids_lec[i] = i + 1;
+        pthread_create(&lecs[i], NULL, lec, &ids_lec[i]);
     }
 
-   
-    pthread_mutex_init(&mutex, NULL);
-    sem_init(&room_empty, 0, 1);
-    sem_init(&turnstile, 0, 1);
 
-    pthread_t lecteurs[nb_lecteurs];
-    pthread_t ecrivains[nb_ecrivains];
-
-    
-    for (int i = 0; i < nb_lecteurs; i++) {
-        int* id = malloc(sizeof(int));
-        *id = i;
-        if (pthread_create(&lecteurs[i], NULL, lecteur, id) != 0) {
-            perror("Erreur lors de la création d'un thread lecteur");
-            return EXIT_FAILURE;
-        }
+    for (int i = 0; i < nb_ecr; i++) {
+        ids_ecr[i] = i + 1;
+        pthread_create(&ecr[i], NULL, writer, &ids_ecr[i]);
     }
 
-    
-    for (int i = 0; i < nb_ecrivains; i++) {
-        int* id = malloc(sizeof(int));
-        *id = i;
-        if (pthread_create(&ecrivains[i], NULL, ecrivain, id) != 0) {
-            perror("Erreur lors de la création d'un thread écrivain");
-            return EXIT_FAILURE;
-        }
+    for (int i = 0; i < nb_lec; i++) {
+        pthread_join(lecs[i], NULL);
     }
 
-    
-    for (int i = 0; i < nb_lecteurs; i++) {
-        if (pthread_join(lecteurs[i], NULL) != 0) {
-            perror("Erreur lors de la terminaison d'un thread lecteur");
-            return EXIT_FAILURE;
-        }
+
+    for (int i = 0; i < nb_ecr; i++) {
+        pthread_join(ecr[i], NULL);
     }
 
-    
-    for (int i = 0; i < nb_ecrivains; i++) {
-        if (pthread_join(ecrivains[i], NULL) != 0) {
-            perror("Erreur lors de la terminaison d'un thread écrivain");
-            return EXIT_FAILURE;
-        }
-    }
 
-    
-    pthread_mutex_destroy(&mutex);
-    sem_destroy(&room_empty);
-    sem_destroy(&turnstile);
+    sem_destroy(&mutex_cmp_lec);
+    sem_destroy(&mutex_cmp_ecr);
+    sem_destroy(&critique);
+    sem_destroy(&queue);
 
-    printf("Simulation terminée avec succès.\n");
+    printf("Programme terminé.\n");
     return EXIT_SUCCESS;
 }
