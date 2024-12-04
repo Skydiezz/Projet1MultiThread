aa69fa22f8b181ce81d9029a01f03aa1bc584fea
alvimavila <guilherme.alvimavila@student.uclouvain.be>
2024-12-04 20:25:35 +0100

    perfs.py

---
+++ perfs.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+
+
+csv_file = "results/production_performance.csv"  
+data = pd.read_csv(csv_file)
+
+
+thread_counts = data["Threads"]
+times = data.iloc[:, 1:-1].values  
+averages = data["Average"]
+
+# Calcul des écarts-types pour chaque configuration de threads
+std_devs = [np.std(times_row) for times_row in times]
+
+
+plt.figure(figsize=(10, 6))
+plt.errorbar(thread_counts, averages, yerr=std_devs, fmt='o-', capsize=5, label="Temps d'exécution moyen")
+plt.title("Temps d'exécution en fonction du nombre de threads", fontsize=14)
+plt.xlabel("Nombre de threads", fontsize=12)
+plt.ylabel("Temps d'exécution en secondes", fontsize=12)
+plt.ylim(0, None)
+plt.grid(True, linestyle='--', alpha=0.7)
+plt.legend(fontsize=12)
+
+
+plt.tight_layout()
+plt.show()
